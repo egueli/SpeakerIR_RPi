abbc4792588b7fa8385399317c5ee38bfeb41632
Enrico Gueli <enrico.gueli@gmail.com>
2022-08-28 15:55:31 +0100

    do it 10 times

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import http.client
 
 conn = http.client.HTTPConnection("192.168.0.106")
-conn.request("GET", "/YamahaExtendedControl/v1/main/getStatus")
-res = conn.getresponse()
-print(res.read())
 
-conn.request("GET", "/YamahaExtendedControl/v1/main/setVolume?volume=101")
-res = conn.getresponse()
-print(res.read())
+for i in range(0, 10):
+	print(i)
+	conn.request("GET", "/YamahaExtendedControl/v1/main/getStatus")
+	res = conn.getresponse()
+	print(res.read())
+
+	conn.request("GET", "/YamahaExtendedControl/v1/main/setVolume?volume=101")
+	res = conn.getresponse()
+	print(res.read())
 
 
