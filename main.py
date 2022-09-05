1fec6a3cd0e21fc3fb153510b29fb3026ed1a346
Enrico Gueli <enrico.gueli@gmail.com>
2022-09-05 18:21:34 +0100

    more updates

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import time
 
 conn = http.client.HTTPConnection("192.168.0.106")
 
-for i in range(0, 10):
+total_duration = 0
+num_samples = 50
+
+for i in range(0, num_samples):
 	print(i)
+	start_at = time.time()
 	if i % 2 == 0:
 		conn.request("GET", "/YamahaExtendedControl/v1/main/getStatus")
 	else:
@@ -12,5 +16,13 @@ for i in range(0, 10):
 
 	res = conn.getresponse()
 	json = res.read()
+	
+	end_at = time.time()
+	duration = end_at - start_at
+	total_duration = total_duration + duration
+
+	print(f"duration: {duration*1000}ms")
+
+print(f"average: {total_duration*1000/num_samples}ms")
 
 
