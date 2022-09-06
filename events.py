26c3ed149a5bbd331d10da7120a88e6b2a998c85
Enrico Gueli <enrico.gueli@gmail.com>
2022-09-06 21:26:26 +0200

    fix

--- events.py
+++ events.py
@@ -12,7 +12,7 @@ device = evdev.InputDevice('/dev/input/by-path/platform-ir-receiver@18-event')
 for event in device.read_loop():
 	if event.type != ecodes.EV_KEY:
 		continue
-	
+
 	if event.value not in [KeyEvent.key_down, KeyEvent.key_hold]:
 		continue
 
@@ -24,8 +24,8 @@ for event in device.read_loop():
 		print(f"mute!        {event.timestamp()}")
 	elif event.code == ecodes.KEY_TV:
 		print(f"TV!          {event.timestamp()}")
-	elif event.code == ecodes.KEY_MUTE:
-		print(f"mute!        {event.timestamp()}")
+	elif event.code == ecodes.KEY_BLUE:
+		print(f"blue!        {event.timestamp()}")
 		# print("time %15f type %3d code %3d value %d" % (event.timestamp(), event.type, event.code, event.value))
 
 
