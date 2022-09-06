86bc3a13167fe83a1d336b62dc77485607f26e36
Enrico Gueli <enrico.gueli@gmail.com>
2022-09-06 21:01:25 +0200

    use stable dev path

--- events.py
+++ events.py
@@ -1,6 +1,6 @@
 import evdev
 
-device = evdev.InputDevice('/dev/input/event4')
+device = evdev.InputDevice('/dev/input/by-path/platform-ir-receiver@18-event')
 for event in device.read_loop():
 	#if event.type == evdev.ecodes.EV_KEY:
 		print(evdev.categorize(event))
