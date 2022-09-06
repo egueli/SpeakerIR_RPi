d489034b197971a247459e6e72a2f37385895c93
Enrico Gueli <enrico.gueli@gmail.com>
2022-09-06 21:05:03 +0200

    different print

--- events.py
+++ events.py
@@ -3,6 +3,6 @@ import evdev
 device = evdev.InputDevice('/dev/input/by-path/platform-ir-receiver@18-event')
 for event in device.read_loop():
 	#if event.type == evdev.ecodes.EV_KEY:
-		print(evdev.categorize(event))
+		print(repr(event))
 
 
