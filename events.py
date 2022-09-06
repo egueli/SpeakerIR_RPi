3e134628c54fa7779b853c349b4d61af618d09b8
Enrico Gueli <enrico.gueli@gmail.com>
2022-09-06 21:08:27 +0200

    filter

--- events.py
+++ events.py
@@ -2,8 +2,8 @@ import evdev
 
 device = evdev.InputDevice('/dev/input/by-path/platform-ir-receiver@18-event')
 for event in device.read_loop():
-	#if event.type == evdev.ecodes.EV_KEY:
-		print(repr(event))
+	print(repr(event))
+	if event.type == evdev.ecodes.EV_KEY:
 		print(repr(evdev.events.KeyEvent(event)))
 
 
