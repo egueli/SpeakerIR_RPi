01628f16940ec278c8d730b1e634ae554dd994d7
Enrico Gueli <enrico.gueli@gmail.com>
2022-09-10 16:46:52 +0200

    fix whitespace

--- ir.py
+++ ir.py
@@ -5,31 +5,31 @@ from typing import Iterator, Optional
 import asyncio
 
 class IRCommandSource:
-	def __init__(self):
-		self._device = evdev.InputDevice('/dev/input/by-path/platform-ir-receiver@18-event')
+    def __init__(self):
+        self._device = evdev.InputDevice('/dev/input/by-path/platform-ir-receiver@18-event')
 
-	async def get_commands(self) -> Iterator[Command]:
-		async for event in self._device.async_read_loop():
-			yield self._process_event(event)
+    async def get_commands(self) -> Iterator[Command]:
+        async for event in self._device.async_read_loop():
+            yield self._process_event(event)
 
-	def _process_event(self, event) -> Optional[Command]:
-		if event.type != ecodes.EV_KEY:
-			return None
+    def _process_event(self, event) -> Optional[Command]:
+        if event.type != ecodes.EV_KEY:
+            return None
 
-		if event.value not in [KeyEvent.key_down, KeyEvent.key_hold]:
-			return None
+        if event.value not in [KeyEvent.key_down, KeyEvent.key_hold]:
+            return None
 
-		if event.code == ecodes.KEY_VOLUMEUP:
-			return VolumeUp()
-		elif event.code == ecodes.KEY_VOLUMEDOWN:
-			return None # TODO
-		elif event.code == ecodes.KEY_MUTE:
-			return None # TODO
-		elif event.code == ecodes.KEY_TV:
-			return None # TODO
-		elif event.code == ecodes.KEY_BLUE:
-			return None # TODO
-			# print("time %15f type %3d code %3d value %d" % (event.timestamp(), event.type, event.code, event.value))
+        if event.code == ecodes.KEY_VOLUMEUP:
+            return VolumeUp()
+        elif event.code == ecodes.KEY_VOLUMEDOWN:
+            return None # TODO
+        elif event.code == ecodes.KEY_MUTE:
+            return None # TODO
+        elif event.code == ecodes.KEY_TV:
+            return None # TODO
+        elif event.code == ecodes.KEY_BLUE:
+            return None # TODO
+            # print("time %15f type %3d code %3d value %d" % (event.timestamp(), event.type, event.code, event.value))
 
 async def test_run():
     ir = IRCommandSource()
