7ae42fd282341dab5878d65396982857db879f62
Enrico Gueli <enrico.gueli@gmail.com>
2022-09-13 20:54:37 +0200

    rename function

--- musiccast.py
+++ musiccast.py
@@ -6,24 +6,24 @@ class MusicCast:
 		self._conn = http.client.HTTPConnection("192.168.0.106")
 
 	def get_volume(self):
-		yxc_status = self._do_yxc_request(f"getStatus")
+		yxc_status = self._do_yxc_zone_request(f"getStatus")
 		return yxc_status['volume']
 
 	def set_volume(self, volume):
-		self._do_yxc_request(f"setVolume?volume={volume}")
+		self._do_yxc_zone_request(f"setVolume?volume={volume}")
 
 	def get_is_muted(self):
-		yxc_status = self._do_yxc_request(f"getStatus")
+		yxc_status = self._do_yxc_zone_request(f"getStatus")
 		return yxc_status['mute']
 
 	def set_muted(self, muted):
 		enable = "true" if muted else "false"
-		self._do_yxc_request(f"setMute?enable={enable}")
+		self._do_yxc_zone_request(f"setMute?enable={enable}")
 
 	def power_on(self):
-		self._do_yxc_request(f"setPower?power=on")
+		self._do_yxc_zone_request(f"setPower?power=on")
 
-	def _do_yxc_request(self, query):
+	def _do_yxc_zone_request(self, query):
 		self._conn.request("GET", f"/YamahaExtendedControl/v1/main/{query}")
 		res = self._conn.getresponse()
 		res_body = res.read()
